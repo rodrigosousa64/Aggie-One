faee71c9ab7f9ffcf6226396a56c1a3d0f947601
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-09-07 18:42:57 -0300

    fix: set vite base to /static/ to serve frontend assets through django whitenoise

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/static/',
   plugins: [react()],
   server: {
     host: true, // Exposes to local network (0.0.0.0)
